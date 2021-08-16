0f02f1c131c6f2dd7238cc21d2d51406a1bad976
zhangfengying1017 <zhang-fengying@nhn-st.com>
2021-08-16 10:26:09 +0800

    初始化

---
+++ vue/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-view/>
+</template>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+}
+body {
+  width: 100%;
+  height: 100%;
+  background-color: #f4f4f4;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
